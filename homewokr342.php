a3aa43de58d704c29221a03fa057daadeaf3f8a0
kimmingyeong225 <127706296+kimmingyeong225@users.noreply.github.com>
2023-04-10 11:35:49 +0900

    Add files via upload

---
+++ homewokr342.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<form action="homework342.php" method="post">
+width: <input type="text" name="width"><br>
+height: <input type="text" name="height"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
